36e8d38b955b7837076720d7b14fa686a9d4deb8
Richard Guaman <richardguaman@Richards-MacBook-Pro.local>
2020-02-07 12:33:33 -0600

    Story 1.2 - Print book name after welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,9 +1,28 @@
 package com.twu.biblioteca;
 
+
+import java.util.ArrayList;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Biblioteca! Your one-stop-shop for great book titles in Bangalore!");
+        ArrayList<Book> Library = populateBooks();
+        for(Book book:Library){
+            System.out.println(book.getBookName());
+        }
+    }
 
+    private static ArrayList<Book> populateBooks() {
+        final Book scienceBook = new Book("Biology");
+        final Book computerBook = new Book("Computer Design");
+        final Book animalBook = new Book("All About Animals");
+        return new ArrayList<Book>(){
+            {
+                add(scienceBook);
+                add(computerBook);
+                add(animalBook);
+            }
+        };
     }
 }
